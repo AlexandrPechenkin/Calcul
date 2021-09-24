f02e93e2f59efb6d358eb546c683cfd0bbca0962
6ak4a <nice.pechen@yandex.ru>
2021-09-24 18:46:23 +0700

    Commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,69 +12,70 @@ public class Main {
             if (string.contains("I") || string.contains("V") || string.contains("X")) {
                 if (string.indexOf('+') > 0) {
                     numbers = string.split(("[+]"));
-                    RomanNumeral first = RomanNumeral.valueOf(numbers[0]);
-                    RomanNumeral second = RomanNumeral.valueOf(numbers[1]);
-                    if (first.getValue() > 10 || second.getValue() > 10) {
+                    int first = RomanNumeral.valueOf(numbers[0]).getValue();
+                    int second = RomanNumeral.valueOf(numbers[1]).getValue();
+                    if (first > 10 || second > 10 || first < 1 || second < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
-                    System.out.println(RomanNumeral.values()[(first.getValue() + second.getValue()) - 1]);
-                }
-                else if (string.indexOf('-') > 0) {
+                    System.out.println(RomanNumeral.values()[(first + second) - 1]);
+                } else if (string.indexOf('-') > 0) {
                     numbers = string.split(("[-]"));
-                    RomanNumeral first = RomanNumeral.valueOf(numbers[0]);
-                    RomanNumeral second = RomanNumeral.valueOf(numbers[1]);
-                    if (first.getValue() > 10 || second.getValue() > 10) {
+                    int first = RomanNumeral.valueOf(numbers[0]).getValue();
+                    int second = RomanNumeral.valueOf(numbers[1]).getValue();
+                    if (first > 10 || second > 10 || first < 1 || second < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
-                    System.out.println(RomanNumeral.values()[(first.getValue() - second.getValue()) - 1]);
+                    System.out.println(RomanNumeral.values()[(first - second) - 1]);
                 } else if (string.indexOf('*') > 0) {
                     numbers = string.split(("[*]"));
-                    RomanNumeral first = RomanNumeral.valueOf(numbers[0]);
-                    RomanNumeral second = RomanNumeral.valueOf(numbers[1]);
-                    if (first.getValue() > 10 || second.getValue() > 10) {
+                    int first = RomanNumeral.valueOf(numbers[0]).getValue();
+                    int second = RomanNumeral.valueOf(numbers[1]).getValue();
+                    if (first > 10 || second > 10 || first < 1 || second < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
-                    System.out.println(RomanNumeral.values()[(first.getValue() * second.getValue()) - 1]);
+                    System.out.println(RomanNumeral.values()[(first * second) - 1]);
                 } else if (string.indexOf('/') > 0) {
                     numbers = string.split(("[/]"));
-                    RomanNumeral first = RomanNumeral.valueOf(numbers[0]);
-                    RomanNumeral second = RomanNumeral.valueOf(numbers[1]);
-                    if (first.getValue() > 10 || second.getValue() > 10) {
+                    int first = RomanNumeral.valueOf(numbers[0]).getValue();
+                    int second = RomanNumeral.valueOf(numbers[1]).getValue();
+                    if (first > 10 || second > 10 || first < 1 || second < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
-                    System.out.println(RomanNumeral.values()[(first.getValue() / second.getValue()) - 1]);
+                    System.out.println(RomanNumeral.values()[(first / second) - 1]);
                 } else {
-                    System.out.println("Неверное выражение"); }
-            }
-
-            else {
+                    System.out.println("Неверное выражение");
+                }
+            } else {
 
                 if (string.indexOf('+') > 0) {
                     numbers = string.split("[+]");
-                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10) {
+                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10 ||
+                            Integer.parseInt(numbers[0]) < 1 || Integer.parseInt(numbers[1]) < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
                     System.out.println(Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]));
                 } else if (string.indexOf('-') > 0) {
                     numbers = string.split("[-]");
-                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10) {
+                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10 ||
+                            Integer.parseInt(numbers[0]) < 1 || Integer.parseInt(numbers[1]) < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
                     System.out.println(Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]));
                 } else if (string.indexOf('*') > 0) {
                     numbers = string.split("[*]");
-                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10) {
+                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10 ||
+                            Integer.parseInt(numbers[0]) < 1 || Integer.parseInt(numbers[1]) < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
                     System.out.println(Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]));
                 } else if (string.indexOf('/') > 0) {
                     numbers = string.split("[/]");
-                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10) {
+                    if (Integer.parseInt(numbers[0]) > 10 || Integer.parseInt(numbers[1]) > 10 ||
+                            Integer.parseInt(numbers[0]) < 1 || Integer.parseInt(numbers[1]) < 1) {
                         throw new WrongNumberException("Число не входит в диапозон от 1 до 10");
                     }
                     System.out.println(Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]));
-                }
-                else {
+                } else {
                     System.out.println("Неверное выражение");
                 }
             }
